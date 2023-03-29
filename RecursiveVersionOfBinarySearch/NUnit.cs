1b6e10301fa85de86ef70dd5741f1cd3b3614a2d
kichapinv <kichapinv@gmail.com>
2023-03-29 13:58:56 +0700

    Add project files.

---
+++ RecursiveVersionOfBinarySearch/NUnit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RecursiveVersionOfBinarySearch
+{
+    [TestFixture]
+    public class NUnit
+    {
+
+        [TestCase(new long[] { 2, 4, 4, 6 }, 1, -1)]
+        [TestCase(new long[] { 2, 4, 4, 6 }, 2, -1)]
+        [TestCase(new long[] { 2, 4, 4, 6 }, 3, 0)]
+        [TestCase(new long[] { 2, 4, 4, 6 }, 4, 0)]
+        [TestCase(new long[] { 2, 4, 4, 6 }, 5, 2)]
+        [TestCase(new long[] { 2, 4, 4, 6 }, 6, 2)]
+        [TestCase(new long[0], 1, -1)]
+        [TestCase(new long[] { 2, 4, 4, 6 }, 7, 3)]
+        
+        public void Tests(long[] array, long value, long result)
+        {
+            Assert.AreEqual(result, Program.FindLeftBorder(array, value));
+        } 
+    }
+}
---
+++ RecursiveVersionOfBinarySearch/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        long[] firstExampleArray = new long[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        long[] secondExampleArray = new long[] { 1, 2, 3, 4, 6, 10, 13, 14, 15 };
+        long[] thirdExampleArray = new long[] { 1, 2, 3, 4, 6, 10, 20, 30, 33, 35, 40 };
+        long firstExampleNumber = 2;
+        long secondExampleNumber = 13;
+        long thirdExampleNumber = 4;
+
+        Console.WriteLine(FindLeftBorder(firstExampleArray, firstExampleNumber));
+        Console.WriteLine(FindLeftBorder(secondExampleArray, secondExampleNumber));
+        Console.WriteLine(FindLeftBorder(thirdExampleArray, thirdExampleNumber));
+    }
+
+    public static int FindLeftBorder(long[] arr, long value)
+    {
+        return BinSearchLeftBorder(arr, value, -1, arr.Length);
+    }
+
+    public static int BinSearchLeftBorder(long[] array, long value, int left, int right)
+    {
+        if (array.Length == 0) return -1;
+        if (array[right - 1] < value) return right - 1;
+        if (array[left + 1] >= value) return left;
+        var m = (left + right) / 2;
+        if (array[m] < value)
+            return BinSearchLeftBorder(array, value, m, right);
+        return BinSearchLeftBorder(array, value, left, m);
+    }
+}
